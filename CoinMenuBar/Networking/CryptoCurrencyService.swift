a51e6046759b01bfc4f6219843c4bdadd687ae46
Horváth Balázs <balazs630@icloud.com>
2018-05-05 22:29:30 +0200

    Fix crash if the exchangeRate value is not found in the response json

--- CoinMenuBar/Networking/CryptoCurrencyService.swift
+++ CoinMenuBar/Networking/CryptoCurrencyService.swift
@@ -20,23 +20,25 @@ class CryptoCurrencyService {
         Alamofire.request(requestUrl, method: .get).responseJSON { response in
             switch response.result {
             case .success:
-                let exchangeRate = self.getExchangeRate(json: response.toJson(), from: cryptoCurrency, to: fiatCurrency)
-                success(exchangeRate)
-
+                if let rate = self.getExchangeRate(json: response.toJson(), from: cryptoCurrency, to: fiatCurrency) {
+                    success(rate)
+                }
             case .failure(let error):
                 NSLog(error.localizedDescription)
             }
         }
     }
 
-    private func getExchangeRate(json: NSDictionary, from cryptoCurrency: String, to fiatCurrency: String) -> Double {
+    private func getExchangeRate(json: NSDictionary, from cryptoCurrency: String, to fiatCurrency: String) -> Double? {
         // HACK: The crypto currency value is a key in the json, unable to write a Codable model for the response
         guard let currency = json[cryptoCurrency] as? NSDictionary else {
-            fatalError("Cannot find \(cryptoCurrency) key!")
+            NSLog("Cannot find \(cryptoCurrency) key!")
+            return nil
         }
 
         guard let exchangeRate = currency[fiatCurrency] as? Double else {
-            fatalError("Cannot find \(fiatCurrency) key for \(cryptoCurrency)!")
+            NSLog("Cannot find \(fiatCurrency) key for \(cryptoCurrency)!")
+            return nil
         }
 
         return exchangeRate
